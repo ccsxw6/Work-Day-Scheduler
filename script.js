3428d79b7ae2719860f0e3cf68e90fe1ace0d55a
ccsxw6 <ccsxw6@gmail.com>
2020-12-09 15:35:34 -0600

    messing with adding color to coordinate with time

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
 // WHEN I click into a time block
@@ -10,12 +7,28 @@
 // WHEN I refresh the page
 // THEN the saved events persist
 
+var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+
 $(document).ready(function() {
+    
     // Displays current day in current day id 
     function displayTime() {
-        var time = moment().format('MMMM Do YYYY, h:mm:ss a');
-        $("#currentDay").html(time) 
-        setTimeout(displayTime, 1000);// keeps seconds!
+        $("#currentDay").html(currentTime) 
+        setTimeout(displayTime, 1000);
     }
     displayTime()
+
+    if (currentTime) { //if the current time is the current time, then set time-block class to .future
+        var newColor = $(".time-block").addClass("future")
+        $(".container").append(newColor)
+    }
+
+    //color time blocks
+    //.present .future .past classes in css
+    //var to check if now is past, current, or future
+    // if rowTime = currentTime, then row time gets this styling
+    //else, if rowTime < currentTime, 
+    //else, if rowtime > currentTime, 
+    //OR set first row to c when time is 9am? 
+  
 })
