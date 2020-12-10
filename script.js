267b029e246587a12687a30344c84d6a9a636875
ccsxw6 <ccsxw6@gmail.com>
2020-12-10 14:59:12 -0600

    able to get style in calendar

--- script.js
+++ script.js
@@ -23,16 +23,24 @@ $(document).ready(function() {
         
     });
 
-  //set styling to hours
-      $(".time-block").each(function() {
-      var blockHour = parseInt($(this).attr("id").split("-")[1]);
-      //.attr sets and returns
-      //how does [1] work? I know it logs 9 10 11 12 up to 17
-      //grabbing time-block, then returning id of time-blcok, an example is "hour-9"
-      //then splitting hour-9
-      //is [1] returning [1] of hour-9? , so hour = [0], and 9 = [1]?
-      //parses that into an integer
-      //does "-" take not use - in hour-9? 
-      console.log(blockHour)
-      })
-    })
+    function updateHour() {
+        var currentHour = moment().hours()
+        //set styling to hours
+            $(".time-block").each(function() {
+            var blockHour = parseInt($(this).attr("id").split("-")[1]);
+            //.attr sets and returns
+            //how does [1] work? I know it logs 9 10 11 12 up to 17
+            //grabbing time-block, then returning id of time-blcok, an example is "hour-9"
+            //then splitting hour-9
+            //is [1] returning [1] of hour-9? , so hour = [0], and 9 = [1]?
+            //parses that into an integer
+            //does "-" take not use - in hour-9? 
+
+            if(currentHour > blockHour) {
+                $(".description").addClass("past")
+            }
+
+        })
+    }
+    updateHour()
+})
