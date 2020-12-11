851c6bc5d24e2431ef19dec22bc79a70383afe26
ccsxw6 <ccsxw6@gmail.com>
2020-12-11 12:57:48 -0600

    finishing up script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-    var events = [] //wouldn't have thought of this
-
+    
     
     function displayTime() {
         var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
@@ -8,39 +7,70 @@ $(document).ready(function() {
         setTimeout(displayTime, 1000);
     }
     displayTime() 
-
+    
+    var userInput = [] 
 
     $(".saveBtn").on("click", function() {
-        // get nearby values
         var value = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id"); // parent of savebtn is div w/ id in it saying the hour
-        var dateAdded = moment().format("dddd, MMMM Do"); //sets date user hit enter to dateAdded
+        var time = $(this).parent().attr("id"); 
+        var dateAdded = moment().format("dddd, MMMM Do"); 
 
-        events.push({description: value, time: time, date: dateAdded});
-
-        // save the value in localStorage as time
-        localStorage.setItem("events", JSON.stringify(events));//turn object into string, setting it in local storage
+        userInput.push({description: value, time: time, date: dateAdded});
+        
+        
+        localStorage.setItem("userInput", JSON.stringify(userInput));
         
     });
+    
+    var storedInput = JSON.parse(localStorage.getItem("userInput"));
+
+    if (storedInput !== null) {
+        userInput = storedInput;
+    }
+
+    for(var i = 0; i < userInput.length; i++) {
+        var userDescription = userInput[i].description;
+        $("#" + userInput[i].time).children(".description").text(userDescription);
+      }
 
     function updateHour() {
         var currentHour = moment().hours()
-        //set styling to hours
+        
             $(".time-block").each(function() {
-            var blockHour = parseInt($(this).attr("id").split("-")[1]);
-            //.attr sets and returns
-            //how does [1] work? I know it logs 9 10 11 12 up to 17
-            //grabbing time-block, then returning id of time-blcok, an example is "hour-9"
-            //then splitting hour-9
-            //is [1] returning [1] of hour-9? , so hour = [0], and 9 = [1]?
-            //parses that into an integer
-            //does "-" take not use - in hour-9? 
 
-            if(currentHour > blockHour) {
-                $(".description").addClass("past")
+            var rowHour = parseInt($(this).attr("id").split("-")[1]);
+
+            if(currentHour > rowHour) {
+                $(this).addClass("past")
+            }
+            else if(currentHour === rowHour) {
+                $(this).removeClass("past")
+                $(this).addClass("present")
+                
+            }
+            else {
+                $(this).removeClass("past")
+                $(this).removeClass("present")
+                $(this).addClass("future")
             }
 
         })
     }
     updateHour()
+
+    //keeping calendar updated
+    var remainingSeconds = 10;
+
+    function time() {
+        setInterval(function() {
+            remainingSeconds--;
+
+            if (remainingSeconds === 0) {
+                updateHour();
+                remainingSeconds = 10;
+            }
+        }, 1000)
+    }
+    time()
+    
 })
