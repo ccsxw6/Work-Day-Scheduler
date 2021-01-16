f7902b46d135c10e6eaa4382e838607231a4ecbf
ccsxw6 <ccsxw6@gmail.com>
2021-01-15 23:06:47 -0600

    fixing comments in script.js

--- script.js
+++ script.js
@@ -11,31 +11,27 @@ $(document).ready(function() {
     var userInput = [] 
 
     $(".saveBtn").on("click", function() {
-        var value = $(this).siblings(".description").val(); //value in the description text area, setting that to a var
-        var time = $(this).parent().attr("id"); //id value of the parent div
-        var dateAdded = moment().format("dddd, MMMM Do"); // value of the time
-        //pushing all that info to an array
+        var value = $(this).siblings(".description").val(); 
+        var time = $(this).parent().attr("id"); 
+        var dateAdded = moment().format("dddd, MMMM Do"); 
+      
         userInput.push({description: value, time: time, date: dateAdded});
         
-        //setting array and stringifying it to local storage        
+               
         localStorage.setItem("userInput", JSON.stringify(userInput));
         
     });
-    //getting the info from local storage and destringifying it? 
+
     var storedInput = JSON.parse(localStorage.getItem("userInput"));
 
     if (storedInput !== null) {
         userInput = storedInput;
     }
 
-    // looping through userInput array to set text onto page
+    
     for(var i = 0; i < userInput.length; i++) {
-        var userDescription = userInput[i].description; //grabbing the description of eacher userInput
+        var userDescription = userInput[i].description;
         $("#" + userInput[i].time).children(".description").text(userDescription);
-        //grabbing the #id of time, 
-        // example: $(#hour-10) - you need the # to grab that id 
-        // selecting children of that hour id, selecting .description from the children
-        //changing the text of the description to userDescription
       }
 
     function updateHour() {
@@ -63,7 +59,6 @@ $(document).ready(function() {
     }
     updateHour()
 
-    //keeping calendar updated
     var remainingSeconds = 10;
 
     function time() {
